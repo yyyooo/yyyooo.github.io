a11997871c72969d760b49353a505486acbb4349
yooohooo <yooohooo@>
2021-08-13 22:39:31 +0800

    optimize

--- sw.js
+++
@@ -1,6 +0,0 @@
-console.log("swswsw");
-
-self.addEventListener('fetch', function (event) {
-    console.log("aaaa")
-    event.respondWith(fetch(event.request));
-});
--- sw1.js
+++
@@ -1,6 +0,0 @@
-console.log("swswsw11111");
-
-self.addEventListener('fetch', function (event) {
-    console.log("aaaa1111111")
-    event.respondWith(fetch(event.request));
-});
